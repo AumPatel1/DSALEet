0ae068802bfe49df06e3c00e76904418bd12cec7
aumpatel <op88814@gmail.com>
2025-12-25 20:52:00 -0500

    BFS

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,70 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(scanner);
-        scanner.close();
+import java.util.*;
+
+// Binary Tree Node definition
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
     }
 }
 
-class BinaryTree{
-    public BinaryTree(){}
-    private static class Node{
-        int value;
-        Node left;
-        Node right;
-        public Node(int value){
-            this.value = value;
+// BFS class containing level-order traversal
+class BFS {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
         }
 
-    }
-    private Node root;
-//commit and push
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
 
-
-
-
-    public void populate(Scanner scanner){
-        System.out.println("enter the root node");
-        int value = scanner.nextInt();
-        root = new Node(value);
-        populate(scanner,root);
-    }
-    public void populate(Scanner scanner, Node node){
-        System.out.println("do you want to enter in left of "+ node.value);
-        boolean left = scanner.nextBoolean();
-        if(left){
-            System.out.println("enter the value you want to enter on "+node.value);
-            int val = scanner.nextInt();
-            node.left = new Node(val);
-
-            populate(scanner,node.left);
-        }
-        System.out.println("do you want to enter in right of "+ node.value);
-        boolean right = scanner.nextBoolean();
-        if(right){
-            System.out.println("enter the value you want to enter on "+node.value);
-            int val = scanner.nextInt();
-            node.right = new Node(val);
-
-            populate(scanner,node.right);
-        }
-    }
-
-    public void display(){
-        display(this.root,"");
-    }
-
-    private void display(Node node , String indent){
-        if(node == null){
-            return;
-        }
-        System.out.println(indent + root.value);
-        display(node.left,indent+"\t");
-        display(node.right,indent+"\t");
-    }
-    public void prettyDisplay(){
-        prettyDisplay(root,0);
-    }
-    private void prettyDisplay(Node node , int level){
-        if(node == null){
-            return;
-        }
-        prettyDisplay(node.right,level+1);
-        if(level!=0){
-            for(int i =0 ;i<level-1;i++){
-                System.out.println("|------>"+node.value);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
             }
+            result.add(currentLevel);
         }
+        return result;
     }
+}
 
+// Main class to run BFS
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a sample tree:
+        //         1
+        //       /   \
+        //      2     3
+        //     / \   / \
+        //    4  5  6  7
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        BFS bfs = new BFS();
+        List<List<Integer>> result = bfs.levelOrder(root);
+
+        System.out.println("BFS Level Order Traversal:");
+        System.out.println(result);
+    }
 }
